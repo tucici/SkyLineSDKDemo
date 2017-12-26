eb010e1bed204b144b465a824c8df21eed767990
tucici <290255613@qq.com>
2017-12-26 19:04:47 +0800

    v1.0.3

---
+++ SkyLineSDK/SkyLineSDK_demo/SkyLineSDK_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SkyLineSDK_demo
+//
+//  Created by skyline on 2017/12/11.
+//  Copyright © 2017年 skyline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
